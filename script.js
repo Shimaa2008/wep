21508575d194de09b46c44311a65975ea3ced7f6
Shimaa2008 <shimaa.elsaid.taher@gmail.com>
2024-10-05 09:59:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js - JavaScript file for navigation and interactivity
+
+// Function to navigate between pages
+function navigateTo(page) {
+    window.location.href = page;
+  }
+  
+  // Example interactivity for story pages
+  function showAlert(message) {
+    alert(message);
+  }
